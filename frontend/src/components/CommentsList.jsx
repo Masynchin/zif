7fd4a3b34c62edab4481fbd1414a68b7678939f7
Masynchin <masynchin@gmail.com>
2021-10-22 20:14:22 +0300

    use index.jsx in folder-components

--- frontend/src/components/CommentsList.jsx
+++ frontend/src/components/CommentsList.jsx
@@ -1,4 +1,4 @@
-import Comment from "./Comment/Comment";
+import Comment from "./Comment";
 
 export default function CommentsList({ comments }) {
   return comments.map((comment) => <Comment key={comment.id} {...comment} />);
--- frontend/src/components/TopicPage.jsx
+++ frontend/src/components/TopicPage.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import CreateThread from "./CreateThread/Button";
+import CreateThread from "./CreateThread";
 import CommentsList from "./CommentsList";
 
 export default function TopicPage() {
