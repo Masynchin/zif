b6998dc102de5ddbc7e4a54015232aafa11a19c0
Masynchin <masynchin@gmail.com>
2021-11-04 02:20:50 +0300

    delete utils.js since `withReplies` used only by `CommentList`

--- frontend/src/components/CommentsList.jsx
+++ frontend/src/components/CommentsList.jsx
@@ -1,8 +1,23 @@
 import Comment from "./Comment";
-import { withReplies } from "./utils";
 
 export default function CommentsList({ comments }) {
   return withReplies(comments).map((comment) => (
     <Comment key={comment.id} {...comment} />
   ));
+}
+
+function withReplies(comments) {
+  const parentChildren = new Map();
+  comments.forEach(({ id, parent_id }) => {
+    if (!parent_id) {
+      return;
+    }
+    if (!parentChildren.has(parent_id)) {
+      parentChildren.set(parent_id, []);
+    }
+    parentChildren.get(parent_id).push({ id: id });
+  });
+  return comments.map((comment) => {
+    return { ...comment, replies: parentChildren.get(comment.id) || [] };
+  });
 }
