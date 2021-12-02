f355414a0c59f24874346da6d06f1ea39d033a6a
Masynchin <masynchin@gmail.com>
2021-12-02 21:28:24 +0300

    make textarea responsive with `react-textarea-autosize`

--- frontend/src/components/ThreadForm/Form.jsx
+++ frontend/src/components/ThreadForm/Form.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useParams } from "react-router";
 import { useDispatch } from "react-redux";
+import TextareaAutosize from "react-textarea-autosize";
 import { insertTopic } from "../../store/actions/topic";
 
 export default function Form({ closeForm }) {
@@ -19,12 +20,13 @@ export default function Form({ closeForm }) {
 
   return (
     <form className="pt-2" onSubmit={onSubmit}>
-      <textarea
+      <TextareaAutosize
+        autoFocus
         id="commentText"
         placeholder="Напишите стартовый комментарий..."
         className="w-full border border-gray-300 rounded shadow-sm focus:border-red-300 focus:ring-red-300"
         onChange={onChange}
-      ></textarea>
+      ></TextareaAutosize>
       <input
         type="submit"
         className="w-full border border-red-300 rounded cursor-pointer py-1 text-white bg-red-600"
