31af109d007d4cc79b2a10e3b6e307c45c7979f0
Max Smirnov <masynchin@gmail.com>
2022-07-08 09:02:14 +0300

    Fix tests running

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -4,8 +4,8 @@ from api.db import close_test_db, setup_test_db
 
 
 @pytest.fixture(autouse=True)
-async def open_test_db():
+def open_test_db():
     """Создание соединения к чистой тестовой ДБ для каждого теста."""
-    await setup_test_db()
+    setup_test_db()
     yield
-    await close_test_db()
+    close_test_db()
