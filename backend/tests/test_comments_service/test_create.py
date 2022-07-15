2021b4087fefcba2924fbf0398668d1631c8a220
Max Smirnov <masynchin@gmail.com>
2022-07-15 19:44:35 +0300

    Remove schema dependency in service (#4)

--- backend/tests/test_comments_service/test_create.py
+++ backend/tests/test_comments_service/test_create.py
@@ -1,35 +1,34 @@
 import pytest
 
 from api import exceptions
-from api.schemas.comment import CommentCreate
 from api.services import comment_service
 
 
 def test_create_thread_starter_comment():
-    data = CommentCreate(
+    comment = comment_service.create_comment(
         content="Comment content",
         topic="it",
         parent_id=None,
     )
-    comment = comment_service.create_comment(data)
 
-    assert comment.content == data.content
-    assert comment.topic == data.topic
+    assert comment.content == "Comment content"
+    assert comment.topic == "it"
     assert comment.parent_id is None
 
 
 def test_create_reply_comment():
-    comment_data = CommentCreate(content="Comment content", parent_id=None)
-    comment = comment_service.create_comment(comment_data)
+    comment = comment_service.create_comment(
+        content="Comment content", parent_id=None
+    )
 
-    reply_data = CommentCreate(content="Comment content", parent_id=comment.id)
-    reply = comment_service.create_comment(reply_data)
+    reply = comment_service.create_comment(
+        content="Comment content", parent_id=comment.id
+    )
 
-    assert reply.content == reply.content
+    assert reply.content == "Comment content"
     assert reply.parent_id == comment.id
 
 
 def test_reply_to_non_existed_comment():
     with pytest.raises(exceptions.CommentParentDoesNotExist):
-        data = CommentCreate(content="Comment content", parent_id=-1)
-        comment_service.create_comment(data)
+        comment_service.create_comment(content="Comment content", parent_id=-1)
--- backend/tests/test_comments_service/test_get_threads_heads.py
+++ backend/tests/test_comments_service/test_get_threads_heads.py
@@ -1,29 +1,18 @@
-from api.schemas.comment import CommentCreate
 from api.services import comment_service
 
 
 def test_get_threads_heads():
-    datas = [
-        CommentCreate(content="", parent_id=None, topic="it"),
-        CommentCreate(content="", parent_id=None, topic="chaos"),
-        CommentCreate(content="", parent_id=None, topic="watch"),
-    ]
-
-    for data in datas:
-        comment_service.create_comment(data)
+    comment_service.create_comment(content="", parent_id=None, topic="it")
+    comment_service.create_comment(content="", parent_id=None, topic="chaos")
+    comment_service.create_comment(content="", parent_id=None, topic="watch")
 
     heads = comment_service.get_threads_heads()
     assert len(heads) == 3
 
 
 def test_only_threads_heads():
-    datas = [
-        CommentCreate(content="", parent_id=None, topic="it"),
-        CommentCreate(content="", parent_id=1, topic="it"),
-    ]
-
-    for data in datas:
-        comment_service.create_comment(data)
+    comment_service.create_comment(content="", parent_id=None, topic="it")
+    comment_service.create_comment(content="", parent_id=1, topic="it")
 
     heads = comment_service.get_threads_heads()
     assert len(heads) == 1
