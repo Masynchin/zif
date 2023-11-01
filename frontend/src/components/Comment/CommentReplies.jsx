da181d2bb66adae5bcfbf83adbfbf1e1d9f197a0
Max Smirnov <masynchin@gmail.com>
2023-11-01 21:47:37 +0300

    Fix language (#16)

--- frontend/src/components/Comment/CommentReplies.jsx
+++ frontend/src/components/Comment/CommentReplies.jsx
@@ -2,14 +2,19 @@ import { Link } from "react-router-dom";
 
 export default function CommentReplies({ replies }) {
   return (
-    <div className="flex space-x-2">
-      {replies.map((reply) => (
-        <Link
-          key={reply.id}
-          to={`#${reply.id}`}
-          className="text-red-500"
-        >{`#${reply.id}`}</Link>
-      ))}
-    </div>
+    replies && (
+      <small>
+        <span>Ответы:</span>
+        <div className="flex space-x-2">
+          {replies.map((reply) => (
+            <Link
+              key={reply.id}
+              to={`#${reply.id}`}
+              className="text-red-500"
+            >{`#${reply.id}`}</Link>
+          ))}
+        </div>
+      </small>
+    )
   );
 }
